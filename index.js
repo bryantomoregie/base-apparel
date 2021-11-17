0f2da135d7bac348aa2ba07fe3a9d2d43d36b86f
Bryant Omoregie <unclebryant@gmail.com>
2021-11-17 09:15:03 -0600

    Refactored the input to actually be an input element

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("525");
+/*
+If email is not the correct format, outline of the input turns red,
+and we get an error. Please provide valid email, with the error icon. 
+
+
+Change to input
+Get inner html
+Check if valid
+If not, change style and add that icon
+On focus, reset back to original 
+*/
