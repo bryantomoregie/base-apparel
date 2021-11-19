35ee3e930aecf178bc2deaa5234425d69216475f
bryantomoregie <unclebryant@gmail.com>
2021-11-19 14:10:30 -0600

    Different for different screen size

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*
-If email is not the correct format, outline of the input turns red,
-and we get an error. Please provide valid email, with the error icon. 
-
-
-
-
-If not, change style and add that icon
-On focus, reset back to original 
-*/
-
 const sliderInput = document.querySelector(".slider--input");
 const sliderButton = document.querySelector(".slider--icon-arrow");
 const errorIcon = document.querySelector(".slider--error");
@@ -34,11 +23,3 @@ function emailIsValid(email) {
 }
 
 sliderButton.addEventListener("click", () => emailIsValid(input));
-
-/*
-    icon hidden, then shown
-    change it red if false 
-    onchange, turn back to normal
-    onclick, we check and run it back
-
-*/
